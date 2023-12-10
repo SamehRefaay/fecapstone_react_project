a38dca2c3cf82179b325d88cfe11a8ea57e9555a
SamehRefaay <samehrefaay89@gmail.com>
2023-12-10 21:28:27 +0200

    update mongo url passwrod and server url

--- server/db.js
+++ server/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-// username: sa------aay8 password: NTI5MS1zYW1laHJl
-const mongoURI = 'mongodb://root:NTI5MS1zYW1laHJl@127.0.0.1:27017';
+// username: sa------aay89 password: gjLzIAXltftFr1Fc // gjLzIAXltftFr1Fc
+// url mongodb+srv://samehrefaay89:<password>@cluster0.bfsrlxw.mongodb.net/?retryWrites=true&w=majority
+// const mongoURI = 'mongodb://root:<password>@127.0.0.1:27017';
+const mongoURI =
+	'mongodb+srv://samehrefaay89:gjLzIAXltftFr1Fc@cluster0.bfsrlxw.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongo = async retryCount => {
 	const MAX_RETRIES = 3;
