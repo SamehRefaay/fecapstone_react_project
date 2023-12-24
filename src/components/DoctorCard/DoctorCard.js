b07146dfd6a583a080676d8e4354cf6157b3d32d
SamehRefaay <samehrefaay89@gmail.com>
2023-12-24 15:33:19 +0200

    Implement Book/Cancel Appointment functionality

--- src/components/DoctorCard/DoctorCard.js
+++ src/components/DoctorCard/DoctorCard.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './DoctorCard.css';
-// import AppointmentFormIC from '../AppointmentFormIC/AppointmentFormIC';
+import AppointmentForm from '../AppointmentForm/AppointmentForm';
 import { v4 as uuidv4 } from 'uuid';
-
 import male1 from '../../assets/images/male-doctor1.jpeg';
 import male2 from '../../assets/images/male-doctor2.jpeg';
 import male3 from '../../assets/images/male-doctor3.jpeg';
@@ -17,6 +16,7 @@ import female4 from '../../assets/images/female-doctor4.jpeg';
 const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 	const [showModal, setShowModal] = useState(false);
 	const [appointments, setAppointments] = useState([]);
+	const i = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
 
 	const handleBooking = () => {
 		setShowModal(true);
@@ -41,7 +41,6 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 	//  'jiao'||'jessica'||'sarah'||'stephny'||'Elizabeth'||'Emily'||samantha||rachel
 	const getDoctorImage = name => {
 		var doctorsImages = [];
-		const i = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
 		if (
 			name.toLowerCase().includes('jiao') ||
 			name.toLowerCase().includes('jessica') ||
@@ -63,7 +62,7 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 		<div className="doctor-card-container">
 			<div className="doctor-card-details-container">
 				<div className="doctor-card-profile-image-container">
-					<img src={getDoctorImage(name)} alt="" />
+					<img src={getDoctorImage(name)} alt="doctor" />
 				</div>
 				<div className="doctor-card-details">
 					<div className="doctor-card-detail-name">{name}</div>
@@ -111,29 +110,40 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 							style={{ height: '100vh', overflow: 'scroll' }}
 						>
 							<div>
-								<div className="doctor-card-profile-image-container">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="46"
-										height="46"
-										fill="currentColor"
-										class="bi bi-person-fill"
-										viewBox="0 0 16 16"
+								<div
+									className="doctor-info"
+									style={{
+										display: 'flex',
+										flex: 'wrap',
+										maxWidth: '400px',
+										margin: '10px auto',
+										justifyContent: 'space-evenly',
+										alignItems: 'center',
+									}}
+								>
+									<div className="doctor-card-profile-image-container">
+										<img src={getDoctorImage(name)} alt="doctor" />
+									</div>
+									{/* <div
+										style={{
+											height: '130px',
+											width: '1px',
+											backgroundColor: '#ddd',
+										}}
 									>
 										{' '}
-										<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />{' '}
-									</svg>
-								</div>
-								<div className="doctor-card-details">
-									<div className="doctor-card-detail-name">{name}</div>
-									<div className="doctor-card-detail-speciality">
-										{speciality}
-									</div>
-									<div className="doctor-card-detail-experience">
-										{experience} years experience
-									</div>
-									<div className="doctor-card-detail-consultationfees">
-										Ratings: {ratings}
+									</div> */}
+									<div className="doctor-card-details">
+										<div className="doctor-card-detail-name">{name}</div>
+										<div className="doctor-card-detail-speciality">
+											{speciality}
+										</div>
+										<div className="doctor-card-detail-experience">
+											{experience} years experience
+										</div>
+										<div className="doctor-card-detail-consultationfees">
+											Ratings: {ratings}
+										</div>
 									</div>
 								</div>
 							</div>
@@ -141,20 +151,38 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
 							{appointments.length > 0 ? (
 								<>
 									<h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
-									{appointments.map(appointment => (
-										<div className="bookedInfo" key={appointment.id}>
-											<p>Name: {appointment.name}</p>
-											<p>Phone Number: {appointment.phoneNumber}</p>
-											<button onClick={() => handleCancel(appointment.id)}>
-												Cancel Appointment
-											</button>
-										</div>
-									))}
+									{appointments.map(appointment => {
+										const a = appointment.startDate.toString().split(' ');
+										const date = `${a[0]} ${a[1]} ${a[2]} ${a[3]}`;
+										return (
+											<div
+												className="bookedInfo"
+												key={appointment.id}
+												style={{ width: '300px', margin: '50px auto' }}
+											>
+												<p style={{ marginTop: '20px' }}>
+													Name: {appointment.name}
+												</p>
+												<p style={{ marginTop: '20px' }}>
+													Phone Number: {appointment.phoneNumber}
+												</p>
+												<p style={{ marginTop: '20px' }}>Date: {date}</p>
+												<p style={{ marginTop: '20px' }}>
+													Time: {appointment.selectedSlot}
+												</p>
+												<button onClick={() => handleCancel(appointment.id)}>
+													Cancel Appointment
+												</button>
+											</div>
+										);
+									})}
 								</>
 							) : (
-								{
-									/* <AppointmentFormIC doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} /> */
-								}
+								<AppointmentForm
+									doctorName={name}
+									doctorSpeciality={speciality}
+									onSubmit={handleFormSubmit}
+								/>
 							)}
 						</div>
 					)}
